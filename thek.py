d552a353a116c29baceb40020625f803cb8cba14
Feng Zhou <zf.pascal@gmail.com>
2012-01-30 17:04:08 +0800

    command recent added

--- thek.py
+++ thek.py
@@ -3,12 +3,58 @@
 # TODO
 # * find keywords in filename and category based on keywords?
 
-import sys,json,shelve
-import operator
+import sys,json,shelve,operator,time
 from os import path
 from subprocess import Popen
 from glob import glob
 from collections import defaultdict
+
+def days_of_month(year,month):
+	if month == 2 :
+		if year % 4 :
+			return 28 
+		else:
+			if year % 100 :
+				return 29 
+			else:
+				if year % 400 :
+					return 28 
+				else:
+					return 29
+	else:
+		if (month - 1) % 7 % 2 :
+			return 30 
+		else:
+			return 31
+
+def time_diff(start_ts,end_ts):
+	start=time.localtime(start_ts)
+	end=time.localtime(end_ts)
+
+	convertions = {
+		'sec'   : ('min',60),
+		'min'   : ('hour',60),
+		'hour'  : ('mday',24),
+		'mday'  : ('mon',days_of_month(start.tm_year,start.tm_mon)),
+		'mon' : ('year',12),
+	}
+
+	diff= {}
+	for tm in ['year','mon','mday','hour','min','sec']:
+		diff[tm] = getattr(end,"tm_%s" % tm) - getattr(start,"tm_%s" % tm)
+
+	for unit,unit_convertion in convertions.items():
+		upperunit,convertion = unit_convertion
+		if diff[unit] < 0:
+			diff[unit]+=convertion
+			diff[upperunit]-=1
+	return diff;
+
+def time_elapsed_for_human(timestamp):
+	diff = time_diff(timestamp,int(time.time()))
+	for tm in ['year','mon','mday','hour','min','sec']:
+		if diff.get(tm):
+			return "%s %s" % (diff[tm],tm)
 
 class Tehk:
 
@@ -36,6 +82,11 @@ class Tehk:
 		history.update(self.persist.get('history',{}))
 		history[path] +=1
 		self.persist['history']=history
+		recent = {}
+		recent.update(self.persist.get('recent',{}))
+		recent[path] = int(time.time())
+		self.persist['recent']=recent
+
 
 	def cmd_debug(self,key):
 		"list access history"
@@ -47,7 +98,7 @@ class Tehk:
 		if(history):
 			history = sorted(history.iteritems(),key=operator.itemgetter(1),reverse=True)
 			self.numerate(map(lambda (k,f): "%s (%d)" % (path.basename(k).replace('-',' '),f), history),
-                            persist_index=map(lambda (k,f):k, history))
+					persist_index=map(lambda (k,f):k, history))
 
 	def cmd_history_clear(self):
 		"clean access history"
@@ -56,6 +107,14 @@ class Tehk:
 	def cmd_history(self,subcmd='ls'):
 		"list acceess history"
 		return self.call_cmd('history_'+subcmd)
+
+	def cmd_recent(self):
+		"list recent acceessed, order by date, desc"
+		recent = self.persist.get('recent')
+		if recent:
+			sorted_recent = sorted(recent.iteritems(), key=operator.itemgetter(1))
+			self.numerate(map(lambda (k,t): "%s (%s)" % (path.basename(k).replace('-',' '),time_elapsed_for_human(t)), sorted_recent),
+					persist_index=[t for f,t in sorted_recent])
 
 	def cmd_ls(self,cat=''):
 		"list dir"
